e2b534da5823dc75af0aa8c961285d85ea644b3a
xuwentao93 <a15157756529@gmail.com>
2021-02-10 15:41:35 +0800

    完成文件上传的进度条

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useRef } from 'react';
-import './App.css';
+import './app.css';
+import { useEffect } from 'react';
 
 const SIZE = 200 * 1024; // 文件切片后, 每个切片最大的大小, 这里是 200KB.
 function App() {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState({}); // 要切块的文件.
+  const [fileList, setFileList] = useState([]); // 切块后的文件数组.
+  const [progress, setProgress] = useState(0);
 
   const fileDOM = useRef();
 
   const methods = {
-    request(url, method = 'post', data = {}, headers = {}, requestList) { // 自定义 ajax 请求.
+    request(url, method = 'post', data = {}, headers = {}, requestList, onProgress) { // 自定义 ajax 请求.
       return new Promise(resolve => {
         const xhr = new XMLHttpRequest();
+        xhr.upload.onprogress = onProgress; // upload.onprogress 在文件上传完一个才会调用.
         xhr.open(method, url);
         Object.keys(headers).forEach(key =>
           xhr.setRequestHeader(key, headers[key])
@@ -28,7 +33,6 @@ function App() {
     },
     changeFile(e) {
       const [file] = e.target.files;
-      console.log(file);
       setFile(file);
     },
     chunkFile() { // 对文件进行切片.
@@ -42,7 +46,7 @@ function App() {
         chunkList.push(file.slice(cur, cur + SIZE));
         cur += SIZE;
       }
-      let hash;
+      let hash = '';
       for (let i = 0; i < 16; i++) {
         const random = parseInt(Math.random() * 16);
         switch (random) {
@@ -52,18 +56,19 @@ function App() {
           case 13: hash += 'd'; break;
           case 14: hash += 'e'; break;
           case 15: hash += 'f'; break;
-          default: hash += random;
+          default: hash += random + '';
         }
       }
-      return chunkList.map((item, index) => ({
+      const chunkFileList = chunkList.map((item, index) => ({
         chunk: item, // 切片后的文件.
         hash: hash + '-' + index // 切片后文件的下标.
       }));
+      setFileList(chunkFileList);
+      return chunkFileList;
     },
     async uploadChunkFile() {
-      const fileChunkList = methods.chunkFile();
-      console.log(fileChunkList);
-      const requestList = fileChunkList.map(({ chunk, hash }) => {
+      // methods.chunkFile();
+      const requestList = fileList.map(({ chunk, hash }) => {
         const data = new FormData();
         data.append('chunk', chunk);
         data.append('hash', hash);
@@ -71,8 +76,8 @@ function App() {
         data.append('filename', file.name);
         return data;
       })
-      .map((data) => methods.request('http://www.xuwentao.com:3456/file/upload', 'post', data));
-      // const finishChunk = await Promise.all(requestList);
+      .map((data, index) => methods.request('http://www.xuwentao.com:3456/file/upload', 'post', data, {}, null, methods.getFileUploadProgress(index)));
+      await Promise.all(requestList); // 等所有文件切块都上传了, 在调用合并接口.
       // console.log(finishChunk);
       methods.request('http://www.xuwentao.com:3456/file/merge', 'post', JSON.stringify({
         fileHash: "unique",
@@ -80,8 +85,27 @@ function App() {
         size: file.size
       }),
       { 'content-type': 'application/json' });
+    },
+    getFileUploadProgress(i) { // 获取文件上传进度.
+      return function(e) {
+        // console.log(e);
+        console.log(e.loaded);
+        // // 没有使用 setFileList 去更新, 不会触发 useEffect.
+        fileList[i].percent = e.total;
+        let curProgress = 0;
+        for (let i = 0; i < fileList.length; i++) {
+          // 0 或者 undefined 都不计算.
+          if (fileList[i].percent) curProgress += fileList[i].percent;
+        }
+        setProgress(parseInt(curProgress * 100 / file.size));
+      };
     }
   };
+
+  useEffect(() => {
+    if (fileList.length === 0) return;
+    methods.uploadChunkFile();
+  }, [fileList]);
 
   return (
     <div className="app">
@@ -90,7 +114,11 @@ function App() {
       <div style={{ margin: '20px 0' }} />
       <div className="button" onClick={methods.chunkFile}>文件切片</div>
       <div style={{ margin: '20px 0' }} />
-      <div className="button" onClick={methods.uploadChunkFile}>文件上传</div>
+      <div className="button" onClick={methods.chunkFile}>文件上传</div>
+      <div className="out-progress">
+        <div className="in-progress" style={{ width: `${progress}%` }} />
+      </div>
+      <div className="progress-brief">{progress}%</div>
     </div>
   );
 }
